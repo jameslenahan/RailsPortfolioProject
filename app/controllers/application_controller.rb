6a187ab8e702ac9ac5bb25ab99eeee47c6fe1800
jameslenahan <57712403+jameslenahan@users.noreply.github.com>
2020-04-20 15:16:13 -0400

    Updated Recipes Controller further
    Create and new functions updated

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,4 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
     request.env['omniauth.origin'] || root_path #saves user location
     end
-  end
 end
---
+++ app/controllers/recipes_controller.rb
@@ -0,0 +1,30 @@
+class RecipesController < ApplicationController
+  before_action :set_recipe, only: [:show, :edit, :update, :destroy]
+  before_action :require_login, only: [:new, :create, :update, :destroy]
+
+  def index
+    if params[:user_id]
+      @recipes = User.find(params[:user_id]).recipes
+    else
+      @recipes = Recipe.all
+    end
+    end
+    def show
+    end
+  def new
+    @recipe = Recipe.new
+    5.times {@recipe.recipe_ingredients.build.build_ingredient}
+  end
+  def edit
+  end
+  def create
+    @recipe = current_user.recipes.build(recipe_params)
+    if @recipe.save
+      redirect_to recipe_path(@recipe)
+    end
+  else
+    5.times {@recipe.recipe_ingredients.build.build_ingredient}
+    render 'new'
+  end
+  
+end
