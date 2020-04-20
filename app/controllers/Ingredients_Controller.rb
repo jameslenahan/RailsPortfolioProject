cd263f5356d9e80aa011d245070d8906d69134c1
jameslenahan <57712403+jameslenahan@users.noreply.github.com>
2020-04-20 12:42:13 -0400

    Update Ingredients_Controller.rb
    Ingredients controller > create methdod first draft done

--- app/controllers/Ingredients_Controller.rb
+++ app/controllers/Ingredients_Controller.rb
@@ -23,8 +23,28 @@ class IngredientsController < ApplicationController
   end
   def edit
   end
-
-
+  def create
+    if ingredient_params[:recipe_id]
+      @recipe = Recipe.find(ingredient_params)[:recipe_id])
+      @ingredient = Ingredient.find_or_create_by(name: ingredient_params[:name])
+      @recipe.recipe_ingredient.build(:ingredient_id => @ingredient.id, :quantity => ingredient_params[:recipe_ingredient][:quantity])
+      if @recipe.save!
+        redirect_to recipe_path(@recipe)
+      else
+        redirect_to new_recipe_ingredient_path(@recipe)
+      end
+    else
+      @ingredient = Ingredient.create(ingredient_params)
+      if @ingredient.save
+        redirect_to ingredients_path
+      else
+        render 'new'
+      end
+    end
   end
-
 end
+  #def update
+  #respond_to do |format|
+  #if @ingredient.update(ingredient_params)
+  ##end
+      # end
