f68b33e4b52c074adca7f27f19ca9a49c96909e8
jameslenahan <57712403+jameslenahan@users.noreply.github.com>
2020-04-20 16:19:17 -0400

    Ingredients Controller update
    Ingredients Controller new & index functions built out

--- app/controllers/Ingredients_Controller.rb
+++ app/controllers/Ingredients_Controller.rb
@@ -1,1 +1,30 @@
+class IngredientsController < ApplicationController
+  before_action :set_ingredient, only: [:show, :edit, :update, :destroy] #set ingredient before changes made, "only" skips this step only for what comes after only
 
+  def index
+    if params[:recipe_id]
+      @ingredients = Recipe.find(params[:recipe_id]).ingredients #if the ingredients are found in index, find them and instantiate into @ingredients
+    else
+      @ingredients = Ingredient.all #or else, create new instance of the ingredients
+
+    end
+  end
+
+  def show
+  end
+  def new
+    if params[:recipe_id]
+      @recipe = Recipe.find(params[:cocktail_id])
+      @recipe_ingredient = @recipe.recipe_ingredients.build #returns one or more objects of "recipes" (collection) that have been instantiated but not yet saved
+      @ingredient = @recipe_ingredient.build_ingredient
+    else
+      @ingredient = Ingredient.new
+    end
+  end
+  def edit
+  end
+
+
+  end
+
+end
---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,8 @@
+class ApplicationController < ActionController::Base
+  protect_from_forgery with :exception #protects from CSRF - cross-site request forgery. i.e. keeping it between user & server
+
+  def after_sign_in_path_for(resource)
+    request.env['omniauth.origin'] || root_path #saves user location
+    end
+  end
+end
